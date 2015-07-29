a41be6f633e591cb100469eb648df9ec01f0c530
pengwan <pwan@aerohive.com>
2015-07-29 11:19:06 +0800

    test computer mac remoter

--- public/js/ah/ah.profile.js
+++ public/js/ah/ah.profile.js
@@ -2,6 +2,10 @@ var profile = (function(){
  
     return {
         resourceTags: {
+			copyOnly : function(filename, mid){
+				return /(png|jpg|jpeg|gif|tiff)$/.test(filename);		
+			},
+
             amd: function(filename, mid){
                 return /\.js$/.test(filename);
                 // If it isn't a test resource, copy only,

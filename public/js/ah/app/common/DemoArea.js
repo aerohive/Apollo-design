cef925433b650cc90e648274e086b057c19e7b99
pengwan <pwan@aerohive.com>
2015-08-07 16:17:35 +0800

    modify

--- public/js/ah/app/common/DemoArea.js
+++ public/js/ah/app/common/DemoArea.js
@@ -1,24 +1,37 @@
 define(["dojo/_base/declare",
 		'dojo/_base/lang',
 		'ah/vendor/highlight',
-	  	"ah/util/common/ModuleBase"], function(declare, lang, hjs, ModuleBase) {
+        'dojo/dom-class',
+	  	"ah/util/common/ModuleBase"], function(declare, lang, hjs, domClass, ModuleBase) {
 
 	return declare("ah/app/common/DemoArea", [ ModuleBase ], {
 
-		templateString : '<div>'+
-                            '<div>${!demoTpl}</div>'+
-                            '<pre data-dojo-attach-point="codeArea">${codeTpl}</pre>'+
+		templateString : '<div class="code-box">'+
+                            '<div class="code-box-demo">${!demoTpl}</div>'+
+                            '<div class="code-box-info">${info}<span data-dojo-attach-point="arrowEl" class="code-box-arrow fa fa-chevron-circle-down"></span></div>'+
+                            '<div style="display:none" data-dojo-attach-point="hjsArea" class="code-box-hjs"><pre data-dojo-attach-point="codeArea">${codeTpl}</pre></div>'+
                         '</div>',
 
-		events : [],
+        info : '',
+
+        arrowDir : 'down',
+
+		events : [
+            ['arrowEl', 'click', '_toggleCodeBox']
+        ],
 
 		postMixInProperties : function(){
 			this.inherited(arguments);
 
-            var firstEl = this.srcNodeRef.firstElementChild;
+            var firstEl = this.srcNodeRef.firstElementChild,
+                secondEl = firstEl.nextElementSibling;
 
 			this.demoTpl = firstEl.outerHTML;
-			this.codeTpl = firstEl.nextElementSibling.outerHTML;
+			this.codeTpl = secondEl.outerHTML;
+
+            if(secondEl.nextElementSibling){
+                this.info = secondEl.nextElementSibling.innerHTML;
+            }
 
             this.srcNodeRef.removeChild(firstEl);
 		},
@@ -27,7 +40,14 @@ define(["dojo/_base/declare",
 			this.inherited(arguments);
 
 			hjs.highlightBlock(this.codeArea);
-		}
+		},
+
+        _toggleCodeBox : function(){
+
+            this.$toggleWipe(this.hjsArea);
+
+            domClass.toggle(this.domNode, 'code-box-rotate');
+        }
 	
 	});
 

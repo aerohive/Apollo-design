a7f4dd048284e8085be7236064b0bc9f64cdcb6f
peng <wanpeng@mantekiiMac.local>
2015-08-12 22:52:44 +0800

    modify

--- public/js/ah/app/common/main.js
+++ public/js/ah/app/common/main.js
@@ -1,3 +1,7 @@
 require(['dojo/parser', 'ah/app/common/home'],function(parser){
 	parser.parse();
+
+    var spinner = document.getElementById('spinner');
+
+    spinner.parentNode.removeChild(spinner);
 })

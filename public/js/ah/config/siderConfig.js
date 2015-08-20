631a7432f8c6df3960f82154d83c1097f31f0235
yaowu <yaowu@aerohive.com>
2015-08-20 13:39:07 +0800

    Tooltip

--- public/js/ah/config/siderConfig.js
+++ public/js/ah/config/siderConfig.js
@@ -19,7 +19,8 @@ define([
 			{label : 'Tab 选项卡', type : '', current : true, widget : 'components/tab'},
 			{label : 'Chosen 选择列表', type : '', widget : 'components/chosen'},
 			{label : 'Carousel 轮播', type : '', widget : 'components/carousel'},
-			{label : 'ObscureInput 密码框', type : '', widget : 'components/obscureInput'}
+			{label : 'ObscureInput 密码框', type : '', widget : 'components/obscureInput'},
+			{label : 'Tooltip 提示框', type : '', widget : 'components/tooltip'}
 		]},
 		{category : 'Styles', list : [
 			//{label : 'Layout 布局', widget : 'styles/layout'},
